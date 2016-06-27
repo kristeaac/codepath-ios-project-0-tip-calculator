a27ecf04c2c01dacc07a2d276bcc350bca1ffe05
Kristy Caster <kristyacaster@gmail.com>
2016-06-26 17:36:06 -0700

    add theme buttons

--- tips/SettingsViewController.swift
+++ tips/SettingsViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    // views
     @IBOutlet weak var defaultTipControl: UISegmentedControl!
+    @IBOutlet weak var yellowThemeButton: UIButton!
+    @IBOutlet weak var greenThemeButton: UIButton!
+    @IBOutlet weak var blueThemeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         defaultTipControl.selectedSegmentIndex = defaultTipIndex()
+        roundButton(yellowThemeButton)
+        roundButton(greenThemeButton)
+        roundButton(blueThemeButton)
+    }
+    
+    private func roundButton(button: UIButton) {
+        button.frame = CGRectMake(100, 100, 100, 100)
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
+        //        view.layer.borderColor = UIColor.whiteColor().CGColor
+        //        view.layer.borderWidth = 1.0
+    }
+    
+    private func round(view: UIView) {
+        view.layer.cornerRadius = view.frame.size.width / 2
+        view.clipsToBounds = true
+//        view.layer.borderColor = UIColor.whiteColor().CGColor
+//        view.layer.borderWidth = 1.0
     }
     
     override func viewWillAppear(animated: Bool) {
--- tips/TipViewController.swift
+++ tips/TipViewController.swift
@@ -45,6 +45,16 @@ class TipViewController: UIViewController {
             hideFields()
             needToRevealFields = true
         }
+        //self.view.backgroundColor = UIColorFromHex(0xffffff)
+        
+    }
+    
+    private func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+        
+        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
     }
 
     private func hideFields() {
